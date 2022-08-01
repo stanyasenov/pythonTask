005842e9c4174799d53a5adef79eebf32d9de330
stanyasenov <mystigal@mail.bg>
2022-08-01 22:11:14 +0300

    color-cells

--- task.py
+++ task.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import pandas as pd
 from datetime import datetime, timedelta, date
 
@@ -9,8 +7,9 @@ def input_keys(data, keys):
     data = list(data.split(' '))
     columns = []
     for value in data:
-        if value in keys:
+        if value in keys and value != 'hu' and value != 'rnr':
             columns.append(value)
+
     return columns
 
 
@@ -33,14 +32,14 @@ def format_color_groups(df):
         return x
     else:
         ''
-# def format_colorcode_groups(df):
-#     if 'labelIds' in header_labels:
-#         x = df.copy()
-#         colors = list(x['labelIds'].unique())
-#         current_date = datetime.now()
-#         factors = list(x['labelIds'].unique())
 
-colored = True
+
+def color_cells(v):
+    colors = set(merged_data['labelIds'])
+    new_colors = [x for x in colors if pd.isnull(x) == False]
+    return f"background-color: {v}" if v in new_colors and v != '' else None
+
+
 today = date.today()
 d1 = today.strftime("%d-%m-%Y")
 
@@ -52,13 +51,13 @@ combined_data =csv_data.merge(api_data_as_json, how='outer')
 
 filtered_data = combined_data.dropna(subset=['hu'])
 filtered_data = filtered_data.sort_values(by='gruppe')
-header_labels = ['rnr']
-header_labels += input_keys(input(), filtered_data)
+header_labels = ['rnr', 'hu']
+input_labels = input_keys(input(), filtered_data)
+header_labels += input_labels
+colored = True
 
 rnr_data_only = []
-
-# change range to filtered_data[header_labels]
-for rnr in range(20, 23):
+for rnr in range(20, 22):
     URL_2 = f'https://api.baubuddy.de/dev/index.php/v1/labels/{rnr}'
     labelid_data_as_json = pd.read_json(URL_2)
     colorcode = None
@@ -66,16 +65,18 @@ for rnr in range(20, 23):
         colorcode = labelid_data_as_json['colorCode'][0]
     rnr_data_only.append({'rnr':rnr, 'labelIds': colorcode})
 
+
 rnr_data_only = pd.DataFrame(rnr_data_only)
 merged_data = filtered_data.merge(rnr_data_only[['labelIds', 'rnr']], on='rnr', how='left')
 merged_data.drop(['labelIds_x'], inplace=True, axis=1)
 merged_data.rename(columns={'labelIds_y':'labelIds'}, inplace=True)
 
-styler = merged_data.style
+styler = merged_data[header_labels].style
 
 if colored:
     styler.apply(format_color_groups, axis=None)
 
+styler.applymap(color_cells)
 
 excelfilename =f'vehicles_{d1}.xlsx'
 styler.to_excel(excelfilename, index=False, columns=header_labels)
